8bcc845d8816033f75139e8b452cc31e11bb7931
suhani <suhani3051rawat@gmail.com>
2026-08-31 21:26:37 +0530

    Add changes

--- client/src/src/api/axios.js
+++ client/src/src/api/axios.js
@@ -4,7 +4,7 @@ import React from 'react';
 const API_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://website-6-2r9x.onrender.com";
+    : "https://website-6-2r9x.onrender.com"; 
 
 export const api = axios.create({
   baseURL: `${API_URL}/admin/Auth`,
